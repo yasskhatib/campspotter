8106ba7eefd72ef54256cb212d33648e0ef837cc
anonyass <anonyass.khatib@gmail.com>
2024-06-16 10:20:05 +0100

    new vercel 7

--- src/components/axiosInstance.js
+++ src/components/axiosInstance.js
@@ -1,9 +1,9 @@
 import axios from 'axios';
 
+console.log('API Base URL:', process.env.REACT_APP_API_BASE_URL); // Add this line to verify
+
 const axiosInstance = axios.create({
     baseURL: process.env.REACT_APP_API_BASE_URL,
-    
 });
-console.log('API Base URL:', process.env.REACT_APP_API_BASE_URL);
 
 export default axiosInstance;
