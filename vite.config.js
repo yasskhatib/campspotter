0e2f29e6749cc72e9ca3021ddaa3c74ae37c23f1
anonyass <anonyass.khatib@gmail.com>
2024-06-17 23:54:43 +0100

    new vercel 27

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import path from 'path'
-
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
-    },
-  },
-  build: {
-    rollupOptions: {
-      external: ['react-dnd', 'react-dnd-html5-backend'],
     },
   },
 })
