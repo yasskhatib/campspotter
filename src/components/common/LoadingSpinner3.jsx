20b7f9427db98715948cca4c1144489f5ad01325
anonyass <anonyass.khatib@gmail.com>
2024-06-17 18:51:36 +0100

    new vercel 25

--- src/components/common/LoadingSpinner3.jsx
+++ src/components/common/LoadingSpinner3.jsx
@@ -3,7 +3,7 @@ import './LoadingSpinner3.css';
 const LoadingSpinner3 = () => {
     return (
         <div className="spinner-container3">
-            <div className="lds-ring">
+            <div className="lds-ring3">
                 <div></div>
                 <div></div>
                 <div></div>
