057c4c442315b5ad41fca78236400c12e2cb1225
Khatib Yassine <anonyass.khatib@gmail.com>
2024-10-14 15:50:09 +0100

    Update axiosInstance.js

--- src/components/axiosInstance.js
+++ src/components/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-    baseURL: 'https://campbackend.vercel.app', //  https://campbackend-production.up.railway.app
+    baseURL: 'https://campbackend-ubg6.onrender.com', //  https://campbackend-production.up.railway.app
 });
 
 export default axiosInstance;
